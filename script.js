fb6cfac070acc777c241a8d7b4b930408cd53d1f
carlomagno-ale <carlomagno.ale@gmail.com>
2025-02-13 16:13:35 +0100

    add if invited (in corso)

--- script.js
+++ script.js
@@ -5,3 +5,22 @@ stampa un messaggio appropriato sull’esito del controllo.
 Lo svolgimento va fatto usando un ciclo for è non usando metodi degli array (come includes, per esempio) . Si puó fare??
 eh si!  Basta ragionare un po’.*/
 
+const invitedList = [
+    'Marco',
+    'Paolo',
+    'Signor Pizza Vegana',
+    'Fabio',
+    'Giuliano',
+    'Alessandro'
+];
+
+const userName = prompt('Inserisci il tuo nome per controllare se sei fra gli invitati:');
+
+for (let i = 0; i < invitedList.length; i++) {
+   
+    if (userName === invitedList[i]) {
+        console.log('sei nella lista');
+    } 
+    
+}
+
